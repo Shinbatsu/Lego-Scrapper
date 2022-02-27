ccfcadbf97f61740b0371a9acc7d1a28a8ff3f5b
Shinbatsu <nojic132@gmail.com>
2022-02-27 15:23:41 +0300

    toy card parser

--- parser.py
+++ parser.py
@@ -13,3 +13,39 @@ def parse_toy_info(info_list: List[str]) -> Dict[str, Optional[str]]:
         else:
             info['pieces'] = item
     return info
+
+
+def parse_price(toy: BeautifulSoup) -> Tuple[str, Optional[str]]:
+    price_text = toy.find('div', {'data-test': 'product-leaf-price-row'}).text
+    price = toy.find('span', {'data-test': 'product-leaf-price'}).text
+
+    if '%' in price_text:
+        discount = toy.find('span', {'data-test': 'product-leaf-discounted-price'}).text
+        return price, discount
+    return price, None
+
+
+def parse_toys(soup: BeautifulSoup, collection: str = 'Marvel') -> List[Dict[str, str]]:
+    toys = soup.find_all('li', {'data-test': 'product-item'})
+    result = []
+
+    for toy in toys:
+        name = toy.find('h3').text.strip()
+
+        attr_tags = toy.find('div', {'data-test': 'product-leaf-attributes-row'}).find_all('span')
+        attributes = [tag.text.strip() for tag in attr_tags]
+        toy_info = parse_toy_info(attributes)
+
+        price, discount = parse_price(toy)
+
+        result.append({
+            'name': name,
+            'collection': collection,
+            'age': toy_info['age'],
+            'pieces': toy_info['pieces'],
+            'rating': toy_info['rating'],
+            'price': price,
+            'discount': discount or '0'
+        })
+
+    return result
