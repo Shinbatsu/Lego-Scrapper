10449ebd326b51a1e5f68f049f473efc4c0073e4
Shinbatsu <nojic132@gmail.com>
2022-03-03 13:08:22 +0300

    util funcs

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ def get_soup(url: str, page: int = 1) -> BeautifulSoup:
     response = requests.get(url)
     response.raise_for_status()
     return BeautifulSoup(response.text, 'lxml')
+
+def save_csv(data: List[Dict], filename: str) -> None:
+    if not data:
+        return
+
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+        
+def get_total_pages(soup: BeautifulSoup) -> Tuple[int, int]:
+    total_items = int(soup.select_one('span[data-value]')['data-value'])
+    pages = (total_items + 17) // 18
+    return total_items, pages
